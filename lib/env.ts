6c44c1977cddf439f9bd3eea47da9134edaf7e3a
Ediz Yurdakul <edizyurdakul@gmail.com>
2026-07-18 23:49:45 +0300

    chore: configure better auth instance and schema generation
    * chore: better auth env variables
    
    * fix: remove dotenv, breaks cli
    
    * chore: configure better auth instance and schema generation

--- lib/env.ts
+++ lib/env.ts
@@ -4,6 +4,8 @@ import { createEnv } from "./create-env";
 export const env = createEnv({
 	server: {
 		DATABASE_URL: z.url(),
+		BETTER_AUTH_URL: z.url(),
+		BETTER_AUTH_SECRET: z.string().min(1),
 		NODE_ENV: z
 			.enum(["development", "production", "test"])
 			.default("development"),
@@ -11,6 +13,8 @@ export const env = createEnv({
 	client: {},
 	runtimeEnv: {
 		DATABASE_URL: process.env.DATABASE_URL,
+		BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+		BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
 		NODE_ENV: process.env.NODE_ENV,
 	},
 	emptyStringAsUndefined: true,
